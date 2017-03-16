3036da35c3eba09b8f4f86a4927284ce90a9cac4
Mwarukason <a.mwaruka93@gmail.com>
2017-03-17 01:03:59 +0800

    modifying and apply new components and states

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -3,22 +3,55 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      headerText: "Welcome to React",
+      contentText: "Mwarukason ReactJS Page"
+    };
+  }
   render() {
-      {/* 
+      
+    { 
+      /* 
           Implementation of JSX
           Adding some javaScriipts and applying into html.
           The declaration occur outside Return and below Render
           NameTags for html re in small and Reacts starts with  Capital
-      */}
+      */
+    }
+
+      {
+        /*
+          Props: properties (create state for each component)
+          created under component and 
+        */
+      }
+
+       {
+        /*
+          The use of props to pass data explained in tabular form
+          used a raw component to pass data created in json array
+        */
+      }
 
     return (
       <div className="App">
-        <Header />
-        <Content />
+       {
+         /*
+       <h1>{this.state.headerText}</h1>
+        <p>{this.state.contentText}</p>
+        */
+       }
+            
+       <Header />
+       <Content />  
       </div>
     );
   }
 }
+
 
 
 class Header extends Component{
@@ -30,27 +63,73 @@ class Header extends Component{
       };
     return(
     <div style={myStyle} className="App-header">
-       <p>   <img src={logo} className="App-logo" alt="logo" />
+          <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
           <h4> Stateless Header Component </h4>
-      </p>
     </div>
     )
   }
 }
 
+
 class Content extends Component{
+  constructor(props){
+    super(props);
+
+    this.state = {
+      data: [
+        {
+          "id": 112112345,
+          "name": "AMRI",
+          "age": "27"
+        },
+        {
+          "id": 112112265,
+          "name": "AQILAH",
+          "age": "22"
+        },
+        {
+          "id": 112112332,
+          "name": "SIHABA",
+          "age": "56"
+        }
+      ]
+    }
+  }
   render(){
-      var i = 1;
+      
     return(
     <p  className="App-intro">
           <h1> Mwarukason ReactJS Page</h1>
           <h4> Stateless Content Component </h4>
+    
+    <table>
+      <thead>
+        <th>
+          <td>ID</td>
+          <td>NAME</td>
+          <td>AGE</td>
+        </th>
+      </thead>
+      <tbody>
+        {this.state.data.map((person, i) => <TableRow key={i} data={person}/>)}
+      </tbody>
+    </table>
     </p>
-        
     )
   }
 }
 
+class TableRow extends Component{
+  render(){      
+    return(
+      <tr>
+        <td>{this.props.data.id}</td>
+        <td>{this.props.data.name}</td>
+        <td>{this.props.data.age}</td>
+      </tr>
+    );
+  }
+}
 
 export default App;
