2c33220246114982aaa2e066acc606853aea7f02
Mwarukason <a.mwaruka93@gmail.com>
2017-03-16 12:53:49 +0800

    working with App.js file learn abt Components and JSX

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -4,18 +4,53 @@ import './App.css';
 
 class App extends Component {
   render() {
+      {/* 
+          Implementation of JSX
+          Adding some javaScriipts and applying into html.
+          The declaration occur outside Return and below Render
+          NameTags for html re in small and Reacts starts with  Capital
+      */}
+
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Header />
+        <Content />
       </div>
     );
   }
 }
+
+
+class Header extends Component{
+  render(){
+        var myStyle = {
+        fontSize: 20,
+        color: 'white',
+        backgroundColor: '#800000'
+      };
+    return(
+    <div style={myStyle} className="App-header">
+       <p>   <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React</h2>
+          <h4> Stateless Header Component </h4>
+      </p>
+    </div>
+    )
+  }
+}
+
+class Content extends Component{
+  render(){
+      var i = 1;
+    return(
+    <p  className="App-intro">
+          <h1> Mwarukason ReactJS Page</h1>
+          <h4> Stateless Content Component </h4>
+    </p>
+        
+    )
+  }
+}
+
 
 export default App;
